7b5ac315bbfa2b2193629ff2b0afbf5e970c3c13
LuLu0616 <32762695+LuLu0616@users.noreply.github.com>
2019-03-24 14:35:27 +0800

    Update 0153_Find_Minimum_in_Rotated_Sorted_Array.py

--- code/0153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ code/0153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -1,19 +1,14 @@
-def find_min_in_rotated_array(nums):
-    p = 0
-    q = len(nums)-1
-    m = int((p + q) / 2)
-    while nums[m] < nums[m+1]:
-        if nums[p] > nums[m]:
-            q = m
-            m = int((p + q) / 2)
-        elif nums[q] < nums[m]:
-            p = m
-            m = int((p + q) / 2)
-        else:
-            return nums[p]
-    return nums[m+1]
-
-
 if __name__ == "__main__":
     nums = [5, 1, 2, 3, 4]
-    print(find_min_in_rotated_array(nums))
+    p = 0
+    q = len(nums) - 1
+    m = 0
+    while p < q:
+        m = p + int((q - p) / 2)
+        if nums[m] > nums[q]:
+            p = m + 1
+        elif nums[m] < nums[q]:
+            q = m
+        else:
+            q = q - 1
+    print(nums[p])
